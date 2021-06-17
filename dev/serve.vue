a968f4eb298a8ff3fdbb4e67133268191b886653
Pooya Golchian <pooya.golchian@gmail.com>
2021-06-17 19:53:07 +0430

    update

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VuetifyFormBaseSsr from '@/vuetify-form-base-ssr.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VuetifyFormBaseSsr
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vuetify-form-base-ssr />
+  </div>
+</template>
